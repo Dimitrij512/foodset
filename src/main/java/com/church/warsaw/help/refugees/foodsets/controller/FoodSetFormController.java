93ba8ae256cb6f96665addbbe1e147c2c509a0d2
dandrusiv <dmytro.andrusiv@gmail.com>
2022-08-24 18:47:24 +0300

    Generate pdf file

--- src/main/java/com/church/warsaw/help/refugees/foodsets/controller/FoodSetFormController.java
+++ src/main/java/com/church/warsaw/help/refugees/foodsets/controller/FoodSetFormController.java
@@ -1,9 +1,15 @@
 package com.church.warsaw.help.refugees.foodsets.controller;
 
+import com.church.warsaw.help.refugees.foodsets.docgenerator.PdfGeneratorService;
 import com.church.warsaw.help.refugees.foodsets.dto.RegistrationInfo;
 import com.church.warsaw.help.refugees.foodsets.service.RegistrationInfoService;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -18,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FoodSetFormController {
 
   private final RegistrationInfoService registrationInfoService;
+
+  private final PdfGeneratorService pdfGeneratorService;
 
   @GetMapping("/food-set-form")
   public String foodSetFormPage() {
@@ -44,6 +52,24 @@ public class FoodSetFormController {
     model.addAttribute("registrationInfos", registrationInfos);
 
     return "registrationInfos";
+  }
+
+  @GetMapping("refugee/registration-infos/generate-pdf-file")
+  public void generatePdfFile(@RequestParam(name = "receiveDate", required = false)
+                              @DateTimeFormat(pattern = "yyyy-mm-dd")
+                              LocalDate receiveDate, HttpServletResponse response) throws IOException {
+
+    LocalDate date = receiveDate == null ? LocalDate.now() : receiveDate;
+    List<RegistrationInfo> registrationInfos = registrationInfoService.getRegistrationInfoByDate(date);
+
+    response.setContentType("application/pdf");
+    DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD:HH:MM:SS");
+
+    String currentDateTime = dateFormat.format(new Date());
+
+    response.setHeader("Content-Disposition", "attachment; filename=registrations-infos" + currentDateTime + ".pdf");
+
+    pdfGeneratorService.generate(registrationInfos, response);
   }
 
 }
---
+++ src/main/java/com/church/warsaw/help/refugees/foodsets/docgenerator/PdfGeneratorService.java
@@ -0,0 +1,101 @@
+package com.church.warsaw.help.refugees.foodsets.docgenerator;
+
+import static com.lowagie.text.Element.ALIGN_CENTER;
+import static java.awt.Color.DARK_GRAY;
+import static java.awt.Color.GRAY;
+
+import com.church.warsaw.help.refugees.foodsets.dto.RegistrationInfo;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfGeneratorService {
+
+  public void generate(List<RegistrationInfo> registrationInfos, HttpServletResponse response)
+      throws DocumentException, IOException {
+
+    try (Document document = new Document(PageSize.A4.rotate())) {
+      PdfWriter.getInstance(document, response.getOutputStream());
+      document.open();
+
+
+      Font fontTittle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+      fontTittle.setSize(20);
+
+      Paragraph paragraph = new Paragraph("List of registrations", fontTittle);
+      paragraph.setAlignment(ALIGN_CENTER);
+      document.add(paragraph);
+
+      PdfPTable table = new PdfPTable(9);
+      table.setHorizontalAlignment(ALIGN_CENTER);
+      table.setWidthPercentage(100);
+      table.setWidths(new int[] {2, 4, 4, 4, 4, 4, 4, 4, 4});
+      table.setSpacingBefore(5);
+
+      // Create Table Cells for the table header
+      PdfPCell cell = new PdfPCell();
+      cell.setBackgroundColor(GRAY.brighter());
+      cell.setHorizontalAlignment(ALIGN_CENTER);
+      cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+      cell.setPadding(5);
+
+      Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+      font.setColor(DARK_GRAY);
+      cell.setPhrase(new Phrase("#", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Date", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Time", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Phone", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Surname", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Name", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Kids", font));
+      table.addCell(cell);
+
+
+      cell.setPhrase(new Phrase("Type", font));
+      table.addCell(cell);
+
+      cell.setPhrase(new Phrase("Received", font));
+      table.addCell(cell);
+
+      int count = 0;
+      for (RegistrationInfo registrationInfo : registrationInfos) {
+
+        table.addCell(String.valueOf(++count));
+        table.addCell(registrationInfo.getDate());
+        table.addCell(registrationInfo.getTime());
+        table.addCell(registrationInfo.getPhoneNumber());
+        table.addCell(registrationInfo.getSurname());
+        table.addCell(registrationInfo.getName());
+        table.addCell(String.valueOf(registrationInfo.getKidsCount()));
+        table.addCell(registrationInfo.getTypeSet());
+        table.addCell(registrationInfo.getIsReceive());
+      }
+      document.add(table);
+    }
+  }
+}
